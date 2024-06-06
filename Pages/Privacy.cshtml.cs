b5c819ef9a449d5a2cb4602e1e1678888cd41362
JosepLlimona <62258739+JosepLlimona@users.noreply.github.com>
2024-06-06 19:45:40 +0200

    fsdafasdfasdgadsfgsdfg

--- Pages/Privacy.cshtml.cs
+++ Pages/Privacy.cshtml.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace udganketo.Pages;
+namespace MyFirstAzureWebApp.Pages;
 
 public class PrivacyModel : PageModel
 {
