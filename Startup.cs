9098ce0ddb803a1492189a5e966af861ea9d957e
JosepLlimona <62258739+JosepLlimona@users.noreply.github.com>
2024-06-06 18:22:36 +0200

    fasdfas

---
+++ Startup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace udganketo
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var cosmosDbConnectionString = Configuration["CosmosDb:ConnectionString"];
+
+            CosmosClient cosmosClient = new CosmosClient(cosmosDbConnectionString);
+            services.AddSingleton(cosmosClient);
+
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+        }
+    }
+}
