cd87d4a7f70887a4e43f04e31ae95190212ab7ed
Roger Roca <u1967076@campus.udg.edu>
2024-06-06 23:11:26 +0200

    Anketo

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddScoped<CosmosDbService>();
-builder.Services.AddSignalR();
+builder.Services.AddSignalR().AddAzureSignalR("Endpoint=https://udganketo.service.signalr.net;AccessKey=uKv5ecO35CwEUASkJhXcwuBI+6d7YjD9nbiHtNrCj4I=;Version=1.0;");
 
 var app = builder.Build();
 
