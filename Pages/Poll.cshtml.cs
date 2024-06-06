29171032a927c74acf787d399d84dbe2feaf2f69
Roger Roca <u1967076@campus.udg.edu>
2024-06-06 22:27:15 +0200

    SignalR client side

---
+++ Pages/Poll.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace udganketo.Pages
+{
+    public class PollModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
