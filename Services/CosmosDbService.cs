976762972c3c57b4218ddcffc67121feaf635486
JosepLlimona <62258739+JosepLlimona@users.noreply.github.com>
2024-06-07 18:18:56 +0200

    fix 2

--- Services/CosmosDbService.cs
+++ Services/CosmosDbService.cs
@@ -82,7 +82,7 @@ namespace udganketo.Services
 
         public async Task UpdateItemAsync(MyItem updatedItem)
         {
-            await _container.ReplaceItemAsync(updatedItem);
+            await _container.ReplaceItemAsync(updatedItem, updatedItem.id);
         }
     }
 
