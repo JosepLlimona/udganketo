9d79848a53212c2efa237d18bb99034cf18e2926
Roger Roca <u1967076@campus.udg.edu>
2024-06-07 18:30:16 +0200

    Update Votacio.cshtml.cs

--- Pages/Votacio.cshtml.cs
+++ Pages/Votacio.cshtml.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.SignalR;
 using SignalRChat.Hubs;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using udganketo.Services;
 
 namespace udganketo.Pages
@@ -26,6 +28,7 @@ namespace udganketo.Pages
         public async Task OnGetAsync(string id)
         {
             Item = await _cosmosDbService.SelectItemAsync(id);
+            await _hubContext.Clients.Group(id).SendAsync("updateItem", Item);
         }
 
         public async Task<IActionResult> OnPostAsync(string id)
@@ -42,43 +45,9 @@ namespace udganketo.Pages
             }
 
             await _cosmosDbService.UpdateItemAsync(Item);
-            await _hubContext.Clients.Group(id).SendAsync("answer", SelectedOptionId);
+            await _hubContext.Clients.Group(id).SendAsync("updateItem", Item);
 
             return RedirectToPage("/Index");
         }
-        /*
-        private readonly CosmosDbService _cosmosDbService;
-
-        public VotacioModel(CosmosDbService cosmosDbService)
-        {
-            _cosmosDbService = cosmosDbService;
-        }
-
-        public MyItem item {  get; private set; }
-
-        public async Task OnGetAsync(string id)
-        {
-            item = await _cosmosDbService.SelectItemAsync(id);
-        }
-
-        public async Task<IActionResult> UpdateItemAsync(string id, string optionId)
-        {
-
-            MyItem item = await _cosmosDbService.SelectItemAsync(id);
-
-            foreach(Options option in item.options)
-            {
-                if (option.id == optionId)
-                {
-                    option.votes++;
-                    break;
-                }
-            }
-
-            await _cosmosDbService.UpdateItemAsync(item);
-
-            return RedirectToPage("/Index");
-        }
-        */
     }
 }
