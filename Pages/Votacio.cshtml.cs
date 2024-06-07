d416a07b70050d041d5a181f4b8e37db1765deb5
Roger Roca <u1967076@campus.udg.edu>
2024-06-07 18:21:18 +0200

    a

--- Pages/Votacio.cshtml.cs
+++ Pages/Votacio.cshtml.cs
@@ -1,11 +1,52 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.SignalR;
+using SignalRChat.Hubs;
 using udganketo.Services;
 
 namespace udganketo.Pages
 {
     public class VotacioModel : PageModel
     {
+        private readonly CosmosDbService _cosmosDbService;
+        private readonly IHubContext<UdgAnketoHub> _hubContext;
+
+        public VotacioModel(CosmosDbService cosmosDbService, IHubContext<UdgAnketoHub> hubContext)
+        {
+            _cosmosDbService = cosmosDbService;
+            _hubContext = hubContext;
+        }
+
+        [BindProperty]
+        public MyItem Item { get; set; }
+
+        [BindProperty]
+        public string SelectedOptionId { get; set; }
+
+        public async Task OnGetAsync(string id)
+        {
+            Item = await _cosmosDbService.SelectItemAsync(id);
+        }
+
+        public async Task<IActionResult> OnPostAsync(string id)
+        {
+            Item = await _cosmosDbService.SelectItemAsync(id);
+
+            foreach (Options option in Item.options)
+            {
+                if (option.id == SelectedOptionId)
+                {
+                    option.votes++;
+                    break;
+                }
+            }
+
+            await _cosmosDbService.UpdateItemAsync(Item);
+            await _hubContext.Clients.Group(id).SendAsync("answer", SelectedOptionId);
+
+            return RedirectToPage("/Index");
+        }
+        /*
         private readonly CosmosDbService _cosmosDbService;
 
         public VotacioModel(CosmosDbService cosmosDbService)
@@ -38,5 +79,6 @@ namespace udganketo.Pages
 
             return RedirectToPage("/Index");
         }
+        */
     }
 }
